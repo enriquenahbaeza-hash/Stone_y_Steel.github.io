ba3145ec2ecbf93387afb04620401f262be039bb
RafaNB <enriquenahbaeza@gmail.com>
2026-07-24 18:17:40 -0600

    Add files via upload

---
+++ miembros/datos.js
@@ -0,0 +1,28 @@
+const listaMiembros = [
+    {
+        id: "eliezer12",
+        gamertag: "@YAMOSSHIJ2",
+        nombreReal: "ELIEZER",
+        rango: "🗡️ Miembro",
+        tiempoMiembro: "Hace 5 años",
+        tiempoJuego: "5 años en Minecraft / 2 en CITYSPOKY",
+        expertoEn: "⚡ DECORADOR Y SUPERVIVENCIA",
+        foto: "fotos/ELIEZER.jpg", // <-- Ruta simplificada
+        descripcion: "Jugador enfocado en  decoracion y recolección de recursos para la comunidad.",
+        experiencias: "miembro destacado en la construcción de estructuras y paisajes."
+    },
+   /* 👇 COPIAS Y PEGAS ESTE BLOQUE AL FINAL Y CAMBIAS SUS DATOS
+    {
+        id: "pedro_craft",
+        gamertag: "@Pedro_Craft",
+        nombreReal: "Pedro Martínez",
+        rango: "👑 Admin",
+        tiempoMiembro: "Hace 1 semana",
+        tiempoJuego: "1 año en Minecraft",
+        expertoEn: "🏹 PvP & Arquería",
+        foto: "fotos/pedro.jpg", // Nombre exacto de su foto
+        descripcion: "Nuevo integrante especializado en combate.",
+        experiencias: "Defensor principal en la última invasión."
+    }
+    hazta aqui 👍*/
+];
